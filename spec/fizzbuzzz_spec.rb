7499056f3caa9a13f350269e1067ad3bb8704555
Iain <iain.hoolahan@gmail.com>
2022-10-04 15:57:46 +0100

    fizzbuzz method changed

--- spec/fizzbuzzz_spec.rb
+++ spec/fizzbuzzz_spec.rb
@@ -3,4 +3,16 @@ describe 'fizzbuzzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+describe 'fizzbuzzz' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+describe 'fizzbuzzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+describe 'fizzbuzzz' do
+  it 'returns "2" when passed 2' do
+    expect(fizzbuzz(2)).to eq '2'
+  end  
 end
