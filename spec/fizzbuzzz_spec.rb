39e287554b18d6d4e47116dcf9a2bfbe8c681331
Alastair <alastair.chau@gmail.com>
2022-10-04 16:18:27 +0100

    updated test

--- spec/fizzbuzzz_spec.rb
+++ spec/fizzbuzzz_spec.rb
@@ -3,16 +3,19 @@ describe 'fizzbuzzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+end
 describe 'fizzbuzzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+end
 describe 'fizzbuzzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+end
 describe 'fizzbuzzz' do
-  it 'returns "2" when passed 2' do
-    expect(fizzbuzz(2)).to eq '2'
-  end  
+  it 'returns 2 when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
+  end
 end
